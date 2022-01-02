755dae39f1aa8b65377678bb48cf7e53fac90262
osman <osman.anr@gmail.com>
2022-01-03 01:10:27 +0300

    first commit

---
+++ lib/detay.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class Detay extends StatefulWidget {
+  // ignore: prefer_typing_uninitialized_variables
+  var imgPath;
+  Detay({Key? key, this.imgPath}) : super(key: key);
+
+  @override
+  _DetayState createState() => _DetayState();
+}
+
+class _DetayState extends State<Detay> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Hero(
+            tag: widget.imgPath,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
+            ),
+          ),
+          Positioned(
+            left: 15,
+            right: 15,
+            bottom: 15,
+            child: Material(
+              borderRadius: BorderRadius.circular(10),
+              elevation: 4,
+              child: Container(
+                height: 240,
+                width: MediaQuery.of(context).size.width - 30,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Container(
+                            height: 120,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: Colors.grey),
+                                image: const DecorationImage(
+                                    image: AssetImage(
+                                      'assets/dress.jpg',
+                                    ),
+                                    fit: BoxFit.contain)),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const <Widget>[
+                            Text(
+                              "LAMINATED",
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Lous vuitton",
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 16,
+                                  color: Colors.grey),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            SizedBox(
+                              height: 30,
+                              width: 220,
+                              child: Text(
+                                "One button V-neck sling long-sleeved waist female stitching dress",
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 13,
+                                    color: Colors.grey),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const Divider(),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 15, top: 10, bottom: 2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          const Text(
+                            "\$6500",
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 22,
+                                color: Colors.grey),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 20),
+                            child: FloatingActionButton(
+                              onPressed: () => {},
+                              child: const Icon(Icons.arrow_forward_ios),
+                              backgroundColor: Colors.brown,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2,
+            left: 50,
+            child: Container(
+              height: 40,
+              width: 130,
+              decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(8)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Center(
+                    child: Text(
+                      "LAMİNATED",
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.white,
+                    size: 18,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
